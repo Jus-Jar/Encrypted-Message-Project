8bad1e01f38c9dd57d0d383731e8860e60d6562a
Jus-Jar <jaredheeralal04@gmail.com>
2023-02-18 13:55:21 -0400

    added frequency json file and modified Part3.py

--- Part3.py
+++ Part3.py
@@ -2,28 +2,33 @@
 # Jared Heeralal (816030252)
 #Dillan Parmanan ()
 
-# Cryptoanalysis 
+import json 
 from Part1 import * 
 
-# Assumptions:
-# RailFence Algorithm Key is 3
+# CryptoAnalysis using frequency analysis (ASCII)
 
+# import data from json file
 
+f_data = json.load(open('ascii_freq.json','r'))
 
+for item in f_data:
+    print(item['Char'])
+    print(item['Freq']) 
+    print("\n")
+
+# Could store data in array and sort in asc or desc order according to frequency
+
+# find the occurences of all characters in rowley.dat and store in array
 filename = "Rowley.dat"
-
-# recognizable_text = ["Rowley","Trinidad","Tobago","murders","Minister","Keith"]
-# encrypted_sample_text = []
-
-# encrypt recognisable text using rail fence algorithm
-
-# obtain text 
-
-
-# doubleCipherEncrypt()
 
 with open (filename, "rb") as file:
     data  = file.read()
 
 
-print (data)
+#sort array in same order as data array and then compare the characters
+
+# swap characters with characters in dataset according to positions in array
+
+# decrypt using railfence algorithm
+
+
